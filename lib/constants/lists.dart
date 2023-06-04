4a5092d30cb5f31899836e306f86b4c45c8f0143
dky278 <divyanshuy2782@gmail.com>
2023-06-04 11:27:25 +0530

    first commit

---
+++ lib/constants/lists.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+List gender = [
+  [false, "Male",],
+  [false, "Female"],
+  [false, "Prefer not to say"],
+];
+
+List hobbies = [
+  [false, "Art and Creativity",],
+  [false, "Sports and Fitness"],
+  [false, "Science and Technology"],
+  [false, "Reading and Writing",],
+  [false, "Music and Performing Arts"],
+  [false, "Other"],
+];
+
+List activities= [
+  [false, "Drawing and Painting",],
+  [false, "Playing Sports"],
+  [false, "Building and Creating"],
+  [false, "Exploring Science",],
+  [false, "Reading Books"],
+  [false, "Playing Music"],
+  [false, "Other"],
+];
+
+List explore= [
+  [false, "Coding and Technology",],
+  [false, "Cooking and Baking"],
+  [false, "Outdoor Adventures"],
+  [false, "Learning a new Language",],
+  [false, "Playing a Musical Instrument"],
+  [false, "Other"],
+];
+
+List subjects= [
+  [false, "Maths",],
+  [false, "Science"],
+  [false, "English/Language Arts"],
+  [false, "Social Studies/History",],
+  [false, "Art"],
+  [false, "Music"],
+  [false, "Physical Education"],
+  [false, "Other"],
+];
